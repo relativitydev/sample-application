95c26c3c96312981b0425ea41491e1951e12224f
Mark Robustelli <mark.robustelli@relativity.com>
2018-06-10 12:26:14 -0500

    initial code commit

---
+++ Source/SampleCustomPage/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using kCura.Relativity.Client;
+using Relativity.API;
+using Relativity.Services.Objects;
+using System;
+using System.Web.Mvc;
+
+namespace SampleCustomPage.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			IAPILog logger = Relativity.CustomPages.ConnectionHelper.Helper().GetLoggerFactory().GetLogger();
+
+			try
+			{
+				//Get the user's first and last name with Relativity API Helpers
+				String firstName = Relativity.CustomPages.ConnectionHelper.Helper().GetAuthenticationManager().UserInfo.FirstName;
+				String lastName = Relativity.CustomPages.ConnectionHelper.Helper().GetAuthenticationManager().UserInfo.LastName;
+
+				//Get the current time
+				String currentTime = DateTime.Now.ToLongTimeString();
+
+				//Set the text value        
+				String helloStr = String.Format("Hello {0} {1}! The current time is: {2}",
+					firstName,
+					lastName,
+					currentTime);
+
+				//Set message
+				ViewBag.Message = helloStr;
+
+				logger.LogVerbose("Log information throughout execution.");
+			}
+			catch (Exception ex)
+			{
+				//Your custom page caught an exception
+				logger.LogError(ex, "There was an exception.");
+			}
+
+			return View();
+		}
+	}
+}
---
+++ Source/SamplePreSaveEventHandler/PreSaveEventHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using kCura.EventHandler;
+using kCura.EventHandler.CustomAttributes;
+using kCura.Relativity.Client;
+using Relativity.API;
+using Relativity.Services.Objects;
+
+namespace SamplePreSaveEventHandler
+{
+	[kCura.EventHandler.CustomAttributes.Description("SamplePreSaveEventHandler")]
+	[System.Runtime.InteropServices.Guid("89207066-FD30-4BF4-816A-01A006806071")]
+	public class PreSaveEventHandler : kCura.EventHandler.PreSaveEventHandler
+	{
+		public override Response Execute()
+		{
+			//Construct a response object with default values.
+			kCura.EventHandler.Response retVal = new kCura.EventHandler.Response();
+			retVal.Success = true;
+			retVal.Message = string.Empty;
+			try
+			{
+				retVal.Success = false;
+				string userName = this.Helper.GetAuthenticationManager().UserInfo.FullName;
+				retVal.Message = String.Format("Hello {0}! You cannot create an object of this type, but that is expected. This is to prove event handler worked.");
+
+				IAPILog logger = Helper.GetLoggerFactory().GetLogger();
+				logger.LogVerbose("Log information throughout execution.");
+			}
+			catch (Exception ex)
+			{
+				//Change the response Success property to false to let the user know an error occurred
+				retVal.Success = false;
+				retVal.Message = ex.ToString();
+			}
+
+			return retVal;
+		}
+
+		/// <summary>
+		///     The RequiredFields property tells Relativity that your event handler needs to have access to specific fields that
+		///     you return in this collection property
+		///     regardless if they are on the current layout or not. These fields will be returned in the ActiveArtifact.Fields
+		///     collection just like other fields that are on
+		///     the current layout when the event handler is executed.
+		/// </summary>
+		public override FieldCollection RequiredFields
+		{
+			get
+			{
+				kCura.EventHandler.FieldCollection retVal = new kCura.EventHandler.FieldCollection();
+				return retVal;
+			}
+		}
+	}
+}
